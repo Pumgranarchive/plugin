5554056773cd061fcdadda1c817c39fcadfd0730
tdjahel <tristan.djahel@epitech.eu>
2015-05-13 21:43:22 +0200

    API : Ajout des deux fonctions : Links_from_content et Links_from_research

--- Node.ts
+++ Node.ts
@@ -4,19 +4,44 @@
 //Links_from_research param: content_uri, research, response: linked_content
 //{ "status":200, "links":[{ "link_uri": 9457, ”content_uri”:65432,"content_title": "Zimbabwe", "content_summary": "my summary" }, { "link__uri": 5, ”content_uri”:143957,"content_title": "Paris”,"content_summary":"my summary"}]}
 
-module PG_API {
-    module Response {
+module Pumgrana {
+    export module DataModel {
+        export interface Response {
+            status: number;
+            error?: string;
+        }
         export interface Link {
             link_uri: string;
-            error?: string;
             content_uri: string;
             content_title: string;
             content_summary: string;
         }
 
-        export interface LinkedContent {
-            status: number;
+        export interface LinkedContent extends Response {
             links: Link[]
+        }
+    }
+    export class API {
+        private NetworkInterface: XMLHttpRequest;
+        constructor ()
+        {
+            this.NetworkInterface = new XMLHttpRequest();
+        }
+        private DoAjaxCall(prefix: string, param: string): DataModel.LinkedContent {
+
+            this.NetworkInterface.open('GET', 'http://api.pumgrana.com/' + prefix, false);
+            this.NetworkInterface.send(encodeURIComponent(param));
+
+            if (this.NetworkInterface.status === 200) {
+                return JSON.parse(this.NetworkInterface.responseText);
+            }
+        }
+        public GetLinksFromContent(uri: string): DataModel.Link[] {
+            return this.DoAjaxCall('link/from_content/', uri).links;
+        }
+
+        public GetLinksFromResearch(input: string) : DataModel.Link[] {
+            return this.DoAjaxCall('link/from_research/', input).links;
         }
     }
 }
