ea90abcc341f000508f969f90312ebd46343272e
Le Bras <leo.lebrasf@gmail.com>
2015-09-29 10:36:02 +0200

    Simplify actions to WebAPI

--- src/scripts/actions/RelatedContentActions.jsx
+++ src/scripts/actions/RelatedContentActions.jsx
@@ -1,5 +1,4 @@
 import * as types from 'constants/ActionTypes';
-
 let WebAPI;
 if(__DEV__){
     WebAPI = require('utils/WebAPI.jsx');
@@ -8,55 +7,23 @@ else{
     WebAPI = require('utils/api.ts').WebAPI;
 }
 
-export function getRelatedContent(pageId, pageUrl) {
+const defaultLimit = 12;
+
+
+
+/**
+ * Get related content
+ *
+ * @param {string} filter
+ * @return {object} promise
+ */
+export function getRelatedContent(page, start = 0, limit = defaultLimit, filter = '') {
     return {
         types: [types.GET_RELATED_CONTENT_REQUEST, types.GET_RELATED_CONTENT_SUCCESS, types.GET_RELATED_CONTENT_ERROR],
-        promise: WebAPI.getRelatedContent(pageId, pageUrl)
-    };
-}
-
-export function loadMoreRelatedContent(pageId, pageUrl, searchFilter) {
-    return {
-        types: [types.LOAD_MORE_RELATED_CONTENT_REQUEST, types.LOAD_MORE_RELATED_CONTENT_SUCCESS, types.LOAD_MORE_RELATED_CONTENT_ERROR],
-        promise: WebAPI.loadMoreRelatedContent(pageId, pageUrl, searchFilter),
-        pageId
-    };
-}
-
-export function searchRelatedContent(pageId, pageUrl, searchFilter) {
-    return {
-        types: [types.SEARCH_RELATED_CONTENT_REQUEST, types.SEARCH_RELATED_CONTENT_SUCCESS, types.SEARCH_RELATED_CONTENT_ERROR],
-        promise: WebAPI.searchRelatedContent(pageId, pageUrl, searchFilter),
-        pageId,
-        searchFilter
-    };
-}
-
-export function resetSearchRelatedContent(pageId) {
-    return {
-        type: types.RESET_SEARCH_RELATED_CONTENT,
-        pageId
-    };
-}
-
-export function getTitle(pageTitle, pageId) {
-    return {
-        type: types.GET_TITLE,
-        pageTitle,
-        pageId
-    };
-}
-
-export function bookmarkRelatedContent(url) {
-    return {
-        type: types.BOOKMARK_RELATED_CONTENT,
-        url
-    };
-}
-
-export function visitRelatedContent(url) {
-    return {
-        type: types.VISIT_RELATED_CONTENT,
-        url
+        promise: WebAPI.getRelatedContent({
+            start,
+            filter,
+            limit
+        })
     };
 }
--- src/scripts/utils/WebAPI.jsx
+++ src/scripts/utils/WebAPI.jsx
@@ -5,79 +5,32 @@ export default {
 	 *
 	 * @return Promise
 	 */
-	getRelatedContent: (pageId, pageUrl) => {
+	getRelatedContent: (pageId) => {
 		return new Promise((resolve) => {
-			resolve([{
-				title: 'Title 1',
-				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
-				website: 'website.com',
-				url: 'http://website.com/article1',
-				tags: ['tag 1', 'tag2'],
-				bookmarked: false,
-				visited: false,
-				pageId: pageId,
-				searchFilter: ''
-	        },
-			{
-				title: 'Title 2',
-				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
-				website: 'website.com',
-				url: 'http://website.com/article2',
-				tags: ['tag 1', 'tag2', 'tag3'],
-				bookmarked: false,
-				visited: false,
-				pageId: pageId,
-				searchFilter: ''
-			}]);
-		})
-	},
-
-
-
-	/**
-	 * Load more related contet
-	 *
-	 * @return Promise
-	 */
-	loadMoreRelatedContent: (pageId, pageUrl, searchFilter) => {
-		return new Promise((resolve, reject) => {
-			// Fake delay to test async
-			setTimeout(() => {resolve([
-                {
-                    title: 'Title 3',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
-                    website: 'website.com',
-                    url: 'http://website.com/article3',
-                    tags: ['tag 1', 'tag2', 'tag3'],
-                    bookmarked: false,
-                    visited: false,
-                    pageId: pageId,
-					searchFilter: searchFilter
-                }
-            ]);}, 2000);
-	    })
-	},
-
-
-
-	/**
-	 * Search related contet
-	 *
-	 * @return Promise
-	 */
-	 searchRelatedContent: (pageId, pageUrl, searchFilter) => {
-    	return new Promise((resolve, reject) => {
-        	resolve([{
-                title: 'Title 3',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
-                website: 'website.com',
-                url: 'http://website.com/article4',
-                tags: ['tag 1', 'tag2', 'tag3'],
-                bookmarked: false,
-                visited: false,
-                pageId: pageId,
-				searchFilter: searchFilter
-            }]);
-		})
+			resolve([
+				{
+					title: 'Title 1',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
+					website: 'website.com',
+					url: 'http://website.com/article1',
+					tags: ['tag 1', 'tag2'],
+					bookmarked: false,
+					visited: false,
+					pageId: pageId,
+					searchFilter: ''
+				},
+				{
+					title: 'Title 2',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue consectetur ante hendrerit ...',
+					website: 'website.com',
+					url: 'http://website.com/article2',
+					tags: ['tag 1', 'tag2', 'tag3'],
+					bookmarked: false,
+					visited: false,
+					pageId: pageId,
+					searchFilter: ''
+				}
+			]);
+		});
 	}
 };
