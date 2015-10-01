600e26b67af047977d971ad7642dd8de98c0dca9
Le Bras <leo.lebrasf@gmail.com>
2015-10-01 23:19:17 +0200

    New <Views /> component

--- src/web_modules/Views/index.jsx
+++ src/web_modules/Views/index.jsx
@@ -1,54 +1,18 @@
-import React, {Component, PropTypes} from 'react';
-import ViewContainer from 'ViewContainer';
-import ViewBookmarksContainer from 'ViewBookmarksContainer';
-import './index.scss';
+import React, { Component } from 'react';
+import styles from './Views';
 
 export default class Views extends Component{
-
-    /**
-     * Props
-     *
-     */
-    static propTypes = {
-        relatedContent: PropTypes.array.isRequired,
-        pages: PropTypes.array.isRequired,
-        currentPage: PropTypes.number.isRequired,
-        goToPage: PropTypes.func.isRequired
-    }
-
-
 
     /**
      * Render
      *
      */
     render(){
-        let { pages, currentPage } = this.props;
-
         return (
-            <div className="Views">
-                <div className="Views_wrapper">
-                    { pages.map((page, i) => {
-                        let position = 'current';
-                        if((currentPage - i) > 0){
-                            position = 'before';
-                        }
-                        else if((currentPage - i) < 0){
-                            position = 'after';
-                        }
-
-                        return (
-                            <ViewContainer
-                                position={ position }
-                                pageId={ i }
-                                pageUrl={ page.url }
-                                key={ i } />
-                        );
-                    }) }
-                    <ViewBookmarksContainer />
-                </div>
+            <div className={ styles.container }>
+                { this.props.children }
             </div>
-
         );
     }
+
 }
