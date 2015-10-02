07d68e166c26d023db8eac5cc30a20e154465d57
Le Bras <leo.lebrasf@gmail.com>
2015-10-02 22:12:08 +0200

    Add <Header />

--- src/web_modules/Header/index.jsx
+++ src/web_modules/Header/index.jsx
@@ -1,61 +1,26 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import ctx from 'classnames';
-import './index.scss';
+import styles from './Header.scss';
 
 export default class Header extends Component{
-
-    /**
-     * Props
-     *
-     */
-    static propTypes = {
-        page: PropTypes.object.isRequired,
-        lastPageId: PropTypes.number.isRequired,
-        goTo: PropTypes.func.isRequired
-    }
-
-
-
-    /**
-     * On click to back
-     *
-     * @return goTo function (props)
-     */
-    back(){
-        return this.props.goTo(this.props.page.id - 1);
-    }
-
-
-
-    /**
-     * On click to next
-     *
-     * @return goTo function (props)
-     */
-    next(){
-        return this.props.goTo(this.props.page.id + 1);
-    }
-
-
 
     /**
      * Render
      *
      */
-    render(){
-        let { page, lastPageId } = this.props;
-
+    render() {
         return (
-            <div className="Header">
-                <a role="button" onClick={ ::this.back } ref="back" aria-label="Go back" href="#/" className={ ctx('Header_back', {
-                    'is-active': page.id !== 0
+            <div className={ styles.container }>
+                <a role="button" className={ ctx({
+                    [styles.back]: true
                 }) }></a>
-                <span className="Header_url">{ page.website }</span>
-                <span className="Header_title">{ page.title }</span>
-                <a role='button' onClick={ ::this.next } ref='next' aria-label='Go next' href='#/' className={ ctx('Header_next', {
-                    'is-active': page.id !== lastPageId
+                <span className={ styles.url }>lemonde.fr</span>
+                <span className={ styles.title }>Le titre de ce super article</span>
+                <a role='button' className={ ctx({
+                    [styles.next]: true
                 }) }></a>
             </div>
         );
     }
+
 }
