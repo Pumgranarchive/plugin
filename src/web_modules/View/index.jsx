cea3d8a0161573a016baa971a07a4ddf47171c6e
Le Bras <leo.lebrasf@gmail.com>
2015-10-02 22:11:45 +0200

    Add <View /> component

--- src/web_modules/View/index.jsx
+++ src/web_modules/View/index.jsx
@@ -1,72 +1,19 @@
-import React, { Component, PropTypes } from 'react';
-import './index.scss';
+import React, { Component } from 'react';
 import Header from 'Header/';
-import SearchBar from 'SearchBar/';
-import List from 'List/';
+import styles from './View.scss';
 
 export default class View extends Component{
 
     /**
-     * Props
-     *
-     */
-    static propTypes = {
-        page: PropTypes.object.isRequired,
-        relatedContent: PropTypes.array.isRequired,
-        position: PropTypes.string.isRequired,
-        goToPage: PropTypes.func.isRequired,
-        lastPageId: PropTypes.number.isRequired,
-        searchFilter: PropTypes.string.isRequired,
-        searchRelatedContent: PropTypes.func.isRequired,
-        resetSearchRelatedContent: PropTypes.func.isRequired,
-        loadMoreRelatedContent: PropTypes.func.isRequired,
-        bookmarkRelatedContent: PropTypes.func.isRequired,
-        visitRelatedContent: PropTypes.func.isRequired
-    }
-
-
-
-    /**
      * Render
      *
-     * @return JSX
+     * @return {JSX}
      */
     render(){
-        let {
-            page,
-            relatedContent,
-            position,
-            lastPageId,
-            goToPage,
-            searchFilter,
-            searchRelatedContent,
-            resetSearchRelatedContent,
-            loadMoreRelatedContent,
-            bookmarkRelatedContent,
-            visitRelatedContent
-        } = this.props;
-
         return (
-            <div className={`View View-${position}`}>
-                <Header
-                    lastPageId={ lastPageId }
-                    goTo={ goToPage }
-                    page={ page } />
-                <div className="View_content">
-                    <h1>
-                       { ((searchFilter === '') ? 'Related content' : `Resulats for "${searchFilter}"`) }
-                    </h1>
-                    <SearchBar
-                        searchRelatedContent={ searchRelatedContent }
-                        resetSearchRelatedContent={ resetSearchRelatedContent } />
-                    <List
-                        page={ page }
-                        relatedContent={ relatedContent }
-                        loadMoreRelatedContent={ loadMoreRelatedContent }
-                        bookmarkRelatedContent={ bookmarkRelatedContent }
-                        visitRelatedContent={ visitRelatedContent }
-                        loadMore={ true } />
-                </div>
+            <div className={ styles.container }>
+                <Header />
+                { this.props.children }
             </div>
         );
     }
