db2ae21a058a7e85b4410d43643c8f0105ad7856
Le Bras <leo.lebrasf@gmail.com>
2015-10-08 16:28:22 +0200

    New <Item /> component

--- src/web_modules/Item/index.jsx
+++ src/web_modules/Item/index.jsx
@@ -1,106 +1,59 @@
 import React, { Component, PropTypes } from 'react';
-import { select, css } from 'utils/dom';
-import './index.scss';
-import ctx from 'classnames';
+import BookmarkIcon from './BookmarkIcon/';
+import styles from './index.scss';
 
 export default class Item extends Component{
 
     /**
-     * Props
+     * State
      *
      */
-    static propTypes = {
-        item: PropTypes.object.isRequired,
-        bookmarkRelatedContent: PropTypes.func.isRequired,
-        visitRelatedContent: PropTypes.func.isRequired
-    }
-
-
-
-    /* ------------------------------------- */
+    state = {
+        hover: false
+    };
 
 
 
     /**
-     * Handle visited
+     * Hover
      *
+     * @return {func} setState()
      */
-    handleVisite(){
-        this.props.visitRelatedContent(this.props.item.url);
-
-        // Remove all elements of the page
-        let selectors = [
-            ...document.querySelectorAll('body > *:not(.Pumgrana__ac863f3)')
-        ];
-        selectors.map((element) => {
-            element.parentNode.removeChild(element);
-        });
-
-        // Set size of body / html
-        select('html')::css({
-            height: '100vh'
-        });
-        select('body')::css({
-            overflow: 'hidden'
-        });
-
-        // Create iframe
-        let webView = document.createElement('iframe');
-        webView.setAttribute('style', 'position: absolute; left: 0; top: 0;');
-        webView.setAttribute('src', 'http://lemonde.fr');
-        webView.setAttribute('width', '100%');
-        webView.setAttribute('height', '100%');
-        webView.setAttribute('frameborder', '0');
-        document.querySelector('body').insertBefore(webView, document.querySelector('.Pumgrana__ac863f3'));
+    onHover() {
+        return this.setState({
+            hover: !this.state.hover
+        })
     }
-
-
-
-    /**
-     * Handle bookmarked
-     *
-     */
-    handleBookmark(){
-        this.props.bookmarkRelatedContent(this.props.item.url);
-    }
-
-
-
-    /* ------------------------------------- */
 
 
 
     /**
      * Render
      *
-     * @return JSX
+     * @return {JSX}
      */
-    render(){
-        let { visited, bookmarked, website, title, description, tags } = this.props.item;
+    render() {
 
         return (
-            <div
-                className={ctx('Item', {
-                    'is-visited': visited
-                })}>
-                <svg onClick={ ::this.handleBookmark } className={
-                    ctx('Item_bookmark', {
-                        'is-active': bookmarked
-                    })
-                } width="15" height="20" viewBox="0 0 15 20" xmlns="http://www.w3.org/2000/svg">
-                    <title>View all your bookmarks</title>
-                    <path d="M3.437 1h8.126L14 3.45V19l-4.063-3.273L5.875 19V3.45L3.437 1 1 3.45V9.18h4.875" stroke="#DCDCDC" fill="#FFFFFF" strokeLinecap="round" strokeLinejoin="round" fillRule="evenodd"/>
-                </svg>
-                <div onClick={ ::this.handleVisite }>
-                    <span className="Item_url">{ website }</span>
-                    <h2 className="Item_title">{ title }</h2>
-                    <p className="Item_description">{ description }</p>
-                    { tags.map((tag, i) => { return (
-                        <span className="Item_tag" key={ i }>{ tag }</span>
+            <div onMouseOver={ ::this.onHover }
+                 onMouseOut={ ::this.onHover }
+                 className={ styles.container }>
+                <BookmarkIcon
+                    show={ this.state.hover } />
+                <div>
+                    <span className={ styles.domainName }>lemonde.fr</span>
+                    <h2 className={ styles.title }>Mon titre à afficher</h2>
+                    <p className={ styles.description }>lorem ipsum</p>
+                    { ['tag 1', 'tag 2', 'tag 3'].map((tag, i) => { return (
+                        <span className={ styles.tag } key={ i }>{ tag }</span>
                     ); }) }
                 </div>
             </div>
         );
     }
+
+}
+
+Item.PropTypes = {
 
 }
