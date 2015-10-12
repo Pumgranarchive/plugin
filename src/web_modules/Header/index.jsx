83fc1a3cd002f87a71e296f226fdc170f05b5136
Le Bras <leo.lebrasf@gmail.com>
2015-10-12 23:15:19 +0200

    Show back and next butons with conditions on <Header />

--- src/web_modules/Header/index.jsx
+++ src/web_modules/Header/index.jsx
@@ -3,10 +3,10 @@ import ctx from 'classnames';
 import styles from './Header.scss';
 import BookmarkIcon from 'BookmarkIcon/';
 
-const Header = ({ title, domainName, bookmarked, bookmarkPage }) => (
+const Header = ({ title, domainName, bookmarked, bookmarkPage, hasAncestors , hasParents, goBack, goNext }) => (
     <div className={ styles.container }>
         <a role="button" className={ ctx({
-            [styles.back]: true
+            [styles.back]: hasAncestors
         }) }></a>
         <span className={ styles.domainName }>{ domainName }</span>
         <span className={ styles.title }>{ title }</span>
@@ -17,7 +17,7 @@ const Header = ({ title, domainName, bookmarked, bookmarkPage }) => (
                 action={ () => bookmarkPage() } />
         </div>
         <a role='button' className={ ctx({
-            [styles.next]: true
+            [styles.next]: hasParents
         }) }></a>
     </div>
 );
