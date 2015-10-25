4bd858f78eb3723bb3522c29a8b59c58f01560f2
Le Bras <leo.lebrasf@gmail.com>
2015-10-25 20:34:45 +0100

    Clean <Overlay /> component

--- src/web_modules/Overlay/index.jsx
+++ src/web_modules/Overlay/index.jsx
@@ -2,37 +2,15 @@ import React, { Component, PropTypes } from 'react';
 import styles from './Overlay.scss';
 import ctx from 'classnames';
 
-export default class Overlay extends Component{
+const Overlay = ({ state, toogleAction }) => (
+    <div className={
+        ctx({
+            [styles.openBox]: (state == 'open'),
+            [styles.closedBox]: (state == 'close')
+        })}
+        style={ ( __DEBUG__ ? { zIndex: 0 } : {} ) }
+        onClick={ e => toogleAction(e) } >
+    </div>
+);
 
-    /**
-     * Render
-     *
-     * @return {JSX}
-     */
-    render() {
-        let { state } = this.props;
-        let zIndex = {};
-        if(__DEBUG__){
-            zIndex = {
-                zIndex: 0
-            };
-        }
-
-        return (
-            <div className={
-                ctx({
-                    [styles.openBox]: (state == 'open'),
-                    [styles.closedBox]: (state == 'close')
-                })}
-                style={ zIndex }
-                onClick={ e => this.props.toogleAction(e) } >
-            </div>
-        );
-    }
-
-}
-
-Overlay.PropTypes = {
-    state: PropTypes.string.isRequired,
-    toogleAction: PropTypes.func.isRequired
-};
+export default Overlay;
