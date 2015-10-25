e1768ca025ec34fc86a417805f54cd9393404dc0
Le Bras <leo.lebrasf@gmail.com>
2015-10-25 20:15:40 +0100

    Fix <ToogleButton />

--- src/web_modules/ToogleButton/index.jsx
+++ src/web_modules/ToogleButton/index.jsx
@@ -2,14 +2,14 @@ import React, {Component, PropTypes} from 'react';
 import ctx from 'classnames';
 import styles from './ToogleButton.scss';
 
-const ToogleButton = ({ state, noResultats }) => (
+const ToogleButton = ({ state, noResultats, action }) => (
     <button
         className={ ctx({
             [styles.openButton]: state == 'open',
             [styles.closedButton]: state == 'close',
             [styles.disabledButton]: !noResultats
         }) }
-        onClick={ () => this.props.action() }>
+        onClick={ () => action() }>
     </button>
 );
 
