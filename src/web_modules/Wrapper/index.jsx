065510b176719d1266d2424e51fdfb7491d2f7dc
Le Bras <leo.lebrasf@gmail.com>
2015-10-25 20:43:40 +0100

    Clean <Wrapper /> component

--- src/web_modules/Wrapper/index.jsx
+++ src/web_modules/Wrapper/index.jsx
@@ -2,35 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import ctx from 'classnames';
 import styles from './Wrapper';
 
-export default class Wrapper extends Component {
+const Wrapper = ({ state, children, noResultats }) => (
+    <div className={ ctx({
+        [styles.openedWrapper]: state == 'open',
+        [styles.closedWrapper]: state == 'close',
+        [styles.disabledWrapper]: !noResultats
+    }) } style={ ( __DEBUG__ ? { zIndex: 0 } : {} ) }>
+        { children }
+    </div>
+);
 
-    /**
-     * Render
-     *
-     * @return {jsx}
-     */
-    render() {
-        let { state, noResultats } = this.props;
-        let zIndex = {};
-        if(__DEBUG__){
-            zIndex = {
-                zIndex: 0
-            };
-        }
-
-        return (
-            <div className={ ctx({
-                [styles.openedWrapper]: state == 'open',
-                [styles.closedWrapper]: state == 'close',
-                [styles.disabledWrapper]: !noResultats
-            }) } style={ zIndex }>
-                { this.props.children }
-            </div>
-        );
-    }
-
-}
-
-Wrapper.PropTypes = {
-    state: PropTypes.string.isRequired
-};
+export default  Wrapper;
