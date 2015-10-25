3b5d6f65e24bb0d801a584b2d3e61bb2f5c54caa
Le Bras <leo.lebrasf@gmail.com>
2015-10-25 20:32:20 +0100

    Clean <Footer /> component

--- src/web_modules/Footer/index.jsx
+++ src/web_modules/Footer/index.jsx
@@ -2,27 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import BookmarksButton from './BookmarksButton/';
 import styles from './Footer';
 
-export default class Footer extends Component{
+const Footer = (props) => (
+     <div className={ styles.container }>
+         <div onClick={ () => props.toogleViewBookmarks() }>
+             <BookmarksButton state={ props.stateViewBookmarks } />
+         </div>
+         <span className={ styles.logo } title="Pumgrana"></span>
+     </div>
+);
 
-    /**
-     * Render
-     *
-     * @return {JSX}
-     */
-     render(){
-         return (
-             <div className={ styles.container }>
-                 <div onClick={ () => this.props.toogleViewBookmarks() }>
-                     <BookmarksButton state={ this.props.stateViewBookmarks } />
-                 </div>
-                 <span className={ styles.logo } title="Pumgrana"></span>
-             </div>
-         );
-     }
-
-}
-
-Footer.propTypes = {
-    toogleViewBookmarks: PropTypes.func.isRequired,
-    stateViewBookmarks: PropTypes.string.isRequired
-};
+export default Footer;
