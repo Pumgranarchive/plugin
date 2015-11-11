d47967ccafc6ebc6f68175665e5251a87bcf04d6
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-11-11 11:43:41 +0100

    Clean <SearchBar />

--- src/web_modules/View/SearchBar/index.jsx
+++ src/web_modules/View/SearchBar/index.jsx
@@ -1,37 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './SearchBar.scss';
 
-export default class SearchBar extends Component{
+const SearchBar = (props) => {
+    let input;
+    const onChange = () => props.searchRelatedContent(input.value);
 
-    /**
-     * onChange()
-     *
-     * @return {func} searchRelatedContent()
-     */
-    onChange(){
-        let search = this.refs.searchBarInput.value;
-        return this.props.searchRelatedContent(search);
-    }
-
-
-
-    /**
-     * Render
-     *
-     * @return JSX
-     */
-    render(){
-        return (
-           <div>
-               <input
-                   type="text"
-                   ref="searchBarInput"
-                   placeholder="Search..."
-                   className={ styles.input }
-                   onChange={ ::this.onChange }
-                   onKeyDown={ ::this.onChange } />
-           </div>
-        );
-    }
-
+    return (
+        <input
+           type="text"
+           ref={ node => input = node }
+           placeholder="Search..."
+           className={ styles.input }
+           onChange={ () => onChange() }
+           onKeyDown={ () => onChange() } />
+    );
 }
+
+export default SearchBar;
