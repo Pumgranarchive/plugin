e05caecfaa273b4cb4d681d8e14b9d9d4fd48c82
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-02 11:30:44 +0100

    Add dev mode for WebAPI

--- src/scripts/utils/WebAPI.jsx
+++ src/scripts/utils/WebAPI.jsx
@@ -12,10 +12,19 @@ export default {
 	 */
 	getRelatedContent: ({url, offset, filter}) => {
 		return new Promise((resolve, reject) => {
-			fetch(`${protocol}${api}linkedcontent/${filter == '' ? 'from_content' : 'search'}/${encodeURIComponent(url)}${filter != '' ? `/${filter}` : ``}`)
-			  .then(response => response.json())
-			  .then(data => resolve((data.links !== null ? data.links : [])))
-			  .catch(errors => reject(errors));
+			if(__DEV__) {
+				return resolve([{
+					content_title: filter == '' ? 'Hello' : filter,
+					content_summary: 'Lorem ipsum dolor',
+					content_uri: 'http://hello.fr/' + filter
+				}])
+			}
+			else {
+				fetch(`${protocol}${api}linkedcontent/${filter == '' ? 'from_content' : 'search'}/${encodeURIComponent(url)}${filter != '' ? `/${filter}` : ``}`)
+				  .then(response => response.json())
+				  .then(data => resolve((data.links !== null ? data.links : [])))
+				  .catch(errors => reject(errors));
+			}
 		});
 	}
 
