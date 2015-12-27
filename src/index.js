b62216384c4249d4e4732adad247a00e71a21ce3
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-12-27 14:29:38 +0100

    Fix render method

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Root from './scripts/containers/Root';
 import 'stylesheets/main';
 import isBlacklisted from 'utils/isBlacklisted'
 
-if(__DEV__ || !isBlacklisted('localhost')) {
+if(__DEV__ || (__PROD__ && !isBlacklisted(location.href))) {
     let el = document.createElement('div');
     el.className = 'Pumgrana__ac863f3';
     document.body.appendChild(el);
