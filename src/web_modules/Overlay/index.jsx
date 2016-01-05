a36ddce24fa3bea34d037e7e9decc7df66825fc8
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-01-05 20:47:49 +0100

    Fix <Overlay />

--- src/web_modules/Overlay/index.jsx
+++ src/web_modules/Overlay/index.jsx
@@ -2,15 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import styles from './Overlay.scss';
 import ctx from 'classnames';
 
-const Overlay = ({ state, toogleAction }) => (
+const Overlay = ({ state, toogleAction }) => {
+    console.log(state);
+    return (
     <div className={
         ctx({
-            [styles.openBox]: (state == 'open'),
-            [styles.closedBox]: (state == 'close')
+            [styles.openBox]: state == 'open',
+            [styles.closedBox]: state == 'close' || state == 'disabled'
         })}
         style={ ( __DEBUG__ ? { zIndex: 0 } : {} ) }
         onClick={ e => toogleAction(e) } >
     </div>
-);
+)};
 
 export default Overlay;
