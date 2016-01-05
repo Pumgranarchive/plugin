796eabcbef863eee74c7054bea68ac4593133b7f
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-01-05 20:40:24 +0100

    Add a button to reactivate the plugin

--- src/web_modules/Popup/index.jsx
+++ src/web_modules/Popup/index.jsx
@@ -48,8 +48,15 @@ export default class Popup extends Component {
             <div className={ styles.container }>
                 <div className={ styles.logo }></div>
                 <div className={ styles.explain }>{ this.state.disabled !== null ? 'Pumgrana est désactivé' : 'Pumgrana est activé'}</div>
-                <button className={ styles.button } onClick={ () => this.onClick(types.DISABLE_FOR_THIS_PAGE) }>Désactiver pour cette page</button>
-                <button className={ styles.button } onClick={ () => this.onClick(types.DISABLE_FOR_THIS_WEBSITE) }>Désactiver pour ce site</button>
+                { this.state.disabled !== null &&
+                    <button className={ styles.button } onClick={ () => this.onClick(types.ENABLE) }>Réactiver</button>
+                }
+                { this.state.disabled === null &&
+                    <div>
+                        <button className={ styles.button } onClick={ () => this.onClick(types.DISABLE_FOR_THIS_PAGE) }>Désactiver pour cette page</button>
+                        <button className={ styles.button } onClick={ () => this.onClick(types.DISABLE_FOR_THIS_WEBSITE) }>Désactiver pour ce site</button>
+                    </div>
+                }
             </div>
         );
     }
